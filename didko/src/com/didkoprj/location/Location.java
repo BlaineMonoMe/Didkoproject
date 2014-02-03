f53e52615ea2f8ea6c1e455931e874c69efcb771
BlaineMonoMe <fallower@ukr.net>
2014-02-03 23:12:20 +0200

    O_o

--- didko/src/com/didkoprj/location/Location.java
+++ didko/src/com/didkoprj/location/Location.java
@@ -1,5 +1,55 @@
 package com.didkoprj.location;
 
-public class Location {
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 
+public class Location implements Screen {
+	private Texture fon = null;
+	
+	public Location(String fonFileName) {
+		if(fonFileName != null) {
+			this.fon = new Texture(fonFileName);
+		}
+		
+	}
+	
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- didko/src/com/didkoprj/main/DidkoGame.java
+++ didko/src/com/didkoprj/main/DidkoGame.java
@@ -3,6 +3,7 @@ package com.didkoprj.main;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,9 +11,12 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.didkoprj.globalMap.GlobalMapGenerator;
 import com.didkoprj.time.TimeWatcher;
 
 public class DidkoGame extends Game {
+	
+
 
 	private DidkoGame(){}
 	
@@ -25,10 +29,11 @@ public class DidkoGame extends Game {
 	
 	@Override
 	public void create() {	
-		for(int i = 0; i < 160; i++) {
+		/*for(int i = 0; i < 160; i++) {
 			System.out.println(TimeWatcher.getString());
 			TimeWatcher.nextTurn();
-		}
+		}*/
+		//GlobalMapGenerator gmg = GlobalMapGenerator.getGlobalMapGenerator();
 		
 	}
 
