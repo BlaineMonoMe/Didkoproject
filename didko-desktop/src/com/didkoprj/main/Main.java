d3b8c7950ef4914903d6fffb12ff0ac3802f91a3
BlaineMonoMe <fallower@ukr.net>
2014-02-03 09:49:08 +0200

    cco

--- didko-desktop/src/com/didkoprj/main/Main.java
+++ didko-desktop/src/com/didkoprj/main/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "didko";
 		cfg.useGL20 = false;
-		cfg.width = 1024;
-		cfg.height = 800;
+		cfg.width = 800;
+		cfg.height = 600;
 		
 		new LwjglApplication(new DidkoGame(), cfg);
 	}
