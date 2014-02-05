3a4c24c406180fda9bf3d01481fa18a4bfadd5bc
BlaineMonoMe <fallower@ukr.net>
2014-02-05 09:13:59 +0200

    simple cell is ready

--- didko/src/com/didkoprj/globalMap/cell/Cell.java
+++ didko/src/com/didkoprj/globalMap/cell/Cell.java
@@ -1,5 +1,15 @@
 package com.didkoprj.globalMap.cell;
 
 public class Cell {
-
+	
+	private CellType type = null;
+	
+	public Cell(CellType type) {
+		this.type = type;
+	}
+	
+	public CellType getType() {
+		return type;
+	}
+	
 }
